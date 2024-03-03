5c5452c5b7ad279aca8e8070f21e25c39d34f7b8
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2024-03-03 10:40:13 +0700

    Add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://remembergaza.id",
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    robotsTxt({
+      sitemap: false,
+    }),
+  ],
+});
