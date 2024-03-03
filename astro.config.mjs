ec4f39600c36d9a916be5fe73fef9ec5b2d37707
Arief Rahmansyah <arief.rahmansyah@gojek.com>
2024-03-03 21:17:51 +0700

    Remove react (astro island)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import partytown from "@astrojs/partytown";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://remembergaza.id",
-  integrations: [partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), robotsTxt({
-    sitemap: false
-  }), react()]
+  integrations: [
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    robotsTxt({
+      sitemap: false,
+    }),
+  ],
 });
